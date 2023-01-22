0eed043d870e8ef060af7f1f8b8db987a22e68a5
imad eddine ZAOUI <imads@imads-MacBook-Pro.local>
2023-01-22 15:05:11 +0100

    more backEnd dev

--- project/controller/update.control.php
+++ project/controller/update.control.php
@@ -5,16 +5,17 @@ include('../Model/update.model.php');
 class updateConfirmation extends update {
     public function changing(){
         
-        if(isset($_POST['productId'])){
-            $productId = $_POST['productId'];
+        if(isset($_POST['id'])){
+            $productId = $_POST['id'];
 
-            $productName = $_POST['productName'];
-            $productPrice = $_POST['productPrice'];
-            $productImage = $_POST['productImage'];
+            $productName = $_POST['name'];
+            $productPrice = $_POST['price'];
+            $productImage = $_POST['image'];
+            $productCategory = $_POST['category'];
             
 
             $executing = new update();
-            $executing->updating($productName,$productPrice,$productImage,$productId);
+            $executing->updating($productName,$productPrice,$productImage,$productCategory,$productId);
 
             header('Location: ../view/dashboard.php');
         }
--- project/model/update.model.php
+++ project/model/update.model.php
@@ -3,13 +3,13 @@ require_once('../Model/database.model.php');
 
 class update extends dataB{
         
-    public function updating($productName,$productPrice,$productImage,$productId){
+    public function updating($productName,$productPrice,$productImage,$productCategory,$productId){
 
         $data = new dataB();
         $pdo=$data->connectPdo();  
 
         $query = "UPDATE `products` 
-                    SET `name` = '$productName', `price` = '$productPrice', `image`='$productImage'
+                    SET `Pimage`='$productImage', `Pname` = '$productName', `Pprice` = '$productPrice', `Pcategory` = '$productCategory'
                     WHERE `id` = '$productId';";
                     
         $exe = $pdo->prepare($query);
